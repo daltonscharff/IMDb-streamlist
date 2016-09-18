2f05b36752bb261981b3249c5ac88c3bdde2da65
Dalton Scharff <dalton.scharff@gmail.com>
2016-09-18 11:47:56 -0400

    Version 1.0: basic watchlist funciton working

---
+++ main.js
@@ -0,0 +1,38 @@
+getMovieTitles();
+
+function getMovieTitles(){
+	var uncutTitles = document.getElementsByClassName("lister-item-header");
+	var years = document.getElementsByClassName("lister-item-year");
+	var titles = [];
+	var iterator = 0;
+	while(uncutTitles[iterator]){
+		var start = uncutTitles[iterator].innerHTML.search(">") + 1;
+		var end = uncutTitles[iterator].innerHTML.search("</a>");
+		titles[iterator] = uncutTitles[iterator].innerHTML.substring(start,end);
+		checkNetflix(titles[iterator], years[iterator].innerHTML, iterator);
+		//console.log(titles[iterator] + " : " + years[iterator].innerHTML);
+		iterator++;
+	}
+}
+
+function checkNetflix(title, year, iterator){
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.open("GET", "http://netflixroulette.net/api/api.php?type=json&title=" + title.replace(/\s/ig, "%20"), true);
+	xmlhttp.onreadystatechange = function() {
+		if (xmlhttp.readyState == 4) {
+			var movie = JSON.parse(xmlhttp.responseText);
+			if(movie.show_id && movie.release_year == year){
+				document.getElementsByClassName("lister-item-header")[iterator].innerHTML += "<span><a href=\"http://www.netflix.com/WiPlayer?movieid=" + movie.show_id + "\" target=\"_blank\" style=\"margin-left: 20px;\"><img style=\"width: 15px;\" src=\"http://i.imgur.com/XfOF49Z.png\"></a></span>";
+			}
+		}
+	};
+	xmlhttp.send();
+}
+
+/*TODO:
+*Add buttons for Hulu and Amazon
+*Add buttons for search pages of IMDb
+*/
+
+//Hulu:http://i.imgur.com/i3zEJV9.png
+//Amazon:http://i.imgur.com/0B4C2Eq.png
